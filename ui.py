a25269c21f94eedc84628196a5e4ef5e4dda33c1
timpo14 <168415187+timpo14@users.noreply.github.com>
2024-06-26 15:18:26 +0300

    Update ui.py

--- ui.py
+++ ui.py
@@ -16,3 +16,17 @@ class Play(pg.sprite.Sprite):
 
         self.image = self.hov if hover else self.org
 
+class Stop(pg.sprite.Sprite):
+    def __init__(self, image):
+        pg.sprite.Sprite.__init__(self)
+
+        self.rect = self.image.get_rect()
+        self.org = pg.image.load("files/sprites/stop.png")
+        self.hov = pg.image.load("files/sprites/stop_hover.png")
+        self.image = image
+    def update(self, events):
+        pos = pygame.mouse.get_pos()
+        hover = self.rect.collidepoint(pos)
+
+        self.image = self.hov if hover else self.org
+
