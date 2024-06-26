ccfd48fc4dc750c930c7c985e89258989facfb11
timpo14 <168415187+timpo14@users.noreply.github.com>
2024-06-26 18:47:39 +0300

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,32 +1,30 @@
 import pygame as pg
 import pygame.event
-from main import *
 
 class Play(pg.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self):
         pg.sprite.Sprite.__init__(self)
 
-        self.rect = self.image.get_rect()
         self.org = pg.image.load("files/sprites/play.png")
         self.hov = pg.image.load("files/sprites/play_hover.png")
-        self.image = image
-    def update(self, events):
+        self.image = self.org
+        self.rect = self.image.get_rect()
+    def update(self):
         pos = pygame.mouse.get_pos()
         hover = self.rect.collidepoint(pos)
 
         self.image = self.hov if hover else self.org
 
 class Stop(pg.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self):
         pg.sprite.Sprite.__init__(self)
 
-        self.rect = self.image.get_rect()
         self.org = pg.image.load("files/sprites/stop.png")
         self.hov = pg.image.load("files/sprites/stop_hover.png")
-        self.image = image
-    def update(self, events):
+        self.image = self.org
+        self.rect = self.image.get_rect()
+    def update(self):
         pos = pygame.mouse.get_pos()
         hover = self.rect.collidepoint(pos)
 
         self.image = self.hov if hover else self.org
-
